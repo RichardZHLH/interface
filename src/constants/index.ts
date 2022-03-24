62d58d8a21f5c2d39047fe21337d7843ab5b9e31
Richard <Richard@wanchain.org>
2022-03-24 19:37:06 +0800

    update

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -22,7 +22,8 @@ const WETH_ONLY: ChainTokenList = {
   [ChainId.ROPSTEN]: [WETH[ChainId.ROPSTEN]],
   [ChainId.RINKEBY]: [WETH[ChainId.RINKEBY]],
   [ChainId.GÖRLI]: [WETH[ChainId.GÖRLI]],
-  [ChainId.KOVAN]: [WETH[ChainId.KOVAN]]
+  [ChainId.KOVAN]: [WETH[ChainId.KOVAN]],
+  [ChainId.PLUTO]: [WETH[ChainId.PLUTO]],
 }
 
 // used to construct intermediary pairs for trading
